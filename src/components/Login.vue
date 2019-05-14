795e95a8fa8466bf1decb94585d59174cecfc802
Ivan Nunez <ivanluis.cba@gmail.com>
2019-05-14 13:29:38 -0300

    Login visual

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,45 @@
 <template>
-  <div id="login">
-    <h1>Login</h1>
+  <div id="app">
+    <v-app id="inspire">
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Login form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <form @submit.prevent="submit">
+                  <v-text-field prepend-icon="person" name="login" label="Login" type="text"/>
+                  <v-text-field
+                    prepend-icon="lock"
+                    name="password"
+                    label="Password"
+                    id="password"
+                    type="password"
+                  />
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" type="submit">Login</v-btn>
+                  </v-card-actions>
+                </form>
+              </v-card-text>
+              
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-app>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Login',
+  methods: {
+    submit() {
+      console.log('submit');
+    }
+  }
 }
 </script>
-
