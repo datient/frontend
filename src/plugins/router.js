d71a1761884528702277b7eea7031db96a91476a
Ivan Nunez <ivanluis.cba@gmail.com>
2019-05-14 13:13:43 -0300

    Crear router

---
+++ src/plugins/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/Login'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/login', component: Login },
+]
+
+export default new VueRouter({
+  routes,
+  mode: 'history'
+})
