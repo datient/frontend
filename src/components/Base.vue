3c5378344a8596df8b1d4386141041584be77ca1
Ivan Nunez <ivanluis.cba@gmail.com>
2019-05-26 19:13:17 -0300

    Create base toolbar

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="base">
+    <Toolbar/>
     <h1>Base</h1>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Toolbar from './Toolbar'
+
 export default {
-  name: 'Base'
+  name: 'Base',
+  components: {
+    Toolbar
+  },
 }
 </script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="toolbar">
+    <v-toolbar color="primary">
+      <v-toolbar-title>
+        Datient
+      </v-toolbar-title>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar',
+}
+</script>
+
+
+<style>
+.v-toolbar__title {
+  color: white;
+}
+</style>
