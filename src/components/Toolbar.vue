844bc7e1d9a9da718a8eac1be50eeff1ec1e396d
Ivan Nunez <ivanluis.cba@gmail.com>
2019-05-27 21:40:50 -0300

    Add logout button in toolbar

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -8,6 +8,24 @@
           Datient
         </router-link>
       </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-menu transition="slide-x-transition">
+          <template v-slot:activator="{ on }">
+            <v-btn dark flat v-on="on">
+              Username
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-tile
+              v-for="(item, index) in items"
+              :key="index"
+              @click="logOut()">
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+        </v-menu>
+      </v-toolbar-items>
     </v-toolbar>
   </div>
 </template>
@@ -15,6 +33,18 @@
 <script>
 export default {
   name: 'Toolbar',
+  data() {
+    return {
+      items: [
+        { title: 'Cerrar sesión' },
+      ]
+    }
+  },
+  methods: {
+    logOut() {
+      this.$router.push({ name: 'login' })
+    }
+  },
 }
 </script>
 
