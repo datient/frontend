01a3f53bb2b71bbdb3fb381a2f800c5392237e56
Benjamin Gilardini <benja.gilardini@gmail.com>
2019-06-14 10:16:05 -0300

    Add card Home and change style

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="home">
-    <h1>Salas</h1>
-    <v-expansion-panel popout focusable>
-      <v-expansion-panel-content
-        v-for="(room, index) in rooms.rooms"
-        :key="index">
-        <template v-slot:header>
-          <div>
-            <h3>{{ room.name }}</h3>
-          </div>
-        </template>
-        <v-card>
-          <v-card-text v-for="(bed, i) in room.beds" :key="i">
-            <v-btn
-              color="primary"
-              flat
-              :to="{ name: 'bed', params: { id: bed.id } }">
-              {{ bed.name }}
-            </v-btn>
-          </v-card-text>
-        </v-card>
-      </v-expansion-panel-content>
-    </v-expansion-panel>
+    <v-card round class="cardSalas">
+      <v-toolbar class="toolbarSalas">
+          <v-toolbar-title class="titleSalas">Salas</v-toolbar-title>
+          <v-spacer></v-spacer>
+      </v-toolbar>
+      <v-expansion-panel popout focusable>
+        <v-expansion-panel-content
+          v-for="(room, index) in rooms.rooms"
+          :key="index">
+          <template v-slot:header>  
+              <div>
+                <h3>{{ room.name }}</h3>
+              </div>
+          </template>
+          <v-card>
+            <v-card-text v-for="(bed, i) in room.beds" :key="i">
+              <v-btn
+                color="primary"
+                flat
+                :to="{ name: 'bed', params: { id: bed.id } }">
+                {{ bed.name }}
+              </v-btn>
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-card>
   </div>
+
 </template>
 
 <script>
@@ -44,4 +50,15 @@ export default {
 h1 {
   text-align: center;
 }
+.titleSalas {
+  color: black;
+}
+.toolbarSalas {
+    display: flex;
+}
+.cardSalas {
+  color: white;
+  border-radius: 10px;
+}
+
 </style>
