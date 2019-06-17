d5b13cb0922660bc1688d8bf7863f622897bd629
Ivan Nuñez <ivanluis.cba@gmail.com>
2019-06-17 13:32:21 -0300

    Add register form and store

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,11 +1,97 @@
 <template>
   <div id="register">
-    <h1>Register</h1>
+    <v-app id="inspire">
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md5>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Register</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <form @submit.prevent="submit">
+                  <v-container grid-list-md>
+                    <v-layout wrap>
+                      <v-flex md6>
+                        <v-text-field
+                          v-model="user.first_name"
+                          prepend-icon="person"
+                          label="Nombre"
+                          type="text"/>
+                      </v-flex>
+                      <v-flex md6>
+                        <v-text-field
+                          v-model="user.last_name"
+                          label="Apellido"
+                          type="text"/>
+                      </v-flex>
+                      <v-flex md12>
+                        <v-text-field
+                          v-model="user.email"
+                          prepend-icon="alternate_email"
+                          label="Email"
+                          type="email"/>
+                      </v-flex>
+                      <v-flex md12>
+                        <v-select
+                          v-model="user.hierarchy"
+                          :items="['Jefe', 'Medico', 'Medico']"
+                          prepend-icon="how_to_reg"
+                          label="Jerarquia">
+                        </v-select>
+                      </v-flex>
+                      <v-flex md6>
+                        <v-text-field
+                          v-model="user.password"
+                          prepend-icon="lock"
+                          label="Password"
+                          type="password"/>
+                      </v-flex>
+                      <v-flex md6>
+                        <v-text-field
+                          v-model="user.password_confirm"
+                          label="Confirmar password"
+                          type="password"/>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" type="submit">Submit</v-btn>
+                  </v-card-actions>
+                </form>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-app>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Register',
+  data() {
+    return {
+      user: {
+        email: null,
+        first_name: null,
+        last_name: null,
+        hierarchy: null,
+        password: null,
+        password_confirm: null,
+      },
+    }
+  },
+  methods: {
+    submit() {
+      this.$store
+        .dispatch('user/createUser', this.user)
+        .then(() => {
+          this.$router.push({ name: 'login' })
+        })
+    }
+  }
 }
 </script>
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -31,6 +31,29 @@ const mutations = {
 }
 
 const actions = {
+  createUser({ commit }, user) {
+    return new Promise((resolve, reject) => {
+      axios({
+        method: 'post',
+        url: 'http://127.0.0.1:8000/accounts/register/',
+        headers: { 'Content-Type': 'application/json' },
+        data: {
+          'email': user.email,
+          'first_name': user.first_name,
+          'last_name': user.last_name,
+          // 'hierarchy': user.hierarchy,
+          'password': user.password,
+          'password_confirm': user.password_confirm
+        }
+      })
+      .then(res => {
+        resolve(res.data)
+      })
+      .catch(err => {
+        reject(err.response)
+      })
+    })
+  },
   logOut({ commit }) {
     commit('setNull')
   },
