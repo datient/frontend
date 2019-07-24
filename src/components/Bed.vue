8a0d3e17414b47b6d4fe04185ef84d0da9e02374
Benjamin Gilardini <benja.gilardini@gmail.com>
2019-07-24 12:43:27 -0300

    Get patients into different beds

--- src/components/Bed.vue
+++ src/components/Bed.vue
@@ -3,13 +3,59 @@
     <v-card round class="cardBed">
       <v-toolbar class="toolbarSalas">
           <v-toolbar-title class="titleSalas">Cama {{ bedId }}</v-toolbar-title>
-      </v-toolbar>
-      <div id="room">
+          <v-card-title>Medico a cargo: {{ user.email }} </v-card-title>
+      <div class="text-xs-center">
+        <v-dialog
+          v-model="dialog"
+          width="500">
+          <v-card>
+            <v-card-title
+              class="headline grey lighten-2"
+              primary-title
+            >
+              Agregar un paciente
+            </v-card-title>
+            <v-container fluid>
+              <v-layout wrap>
+                <v-flex xs12>
+                  <v-combobox
+                    v-model="select"
+                    :items="patient.last_name"
+                    label="Seleccione uno de los pacientes disponibles para anadirlo a la cama"
+                  ></v-combobox>
+                </v-flex>      
+              </v-layout>
+            </v-container>
+           
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="primary"
+                flat
+                @click="dialog = false"
+              >
+                Agregar
+              </v-btn>                                                                                                                                      
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </div>
+    </v-toolbar>
+    <div id="room">
       <v-list>
         <v-list-tile>
           <v-list-tile-content>
             <v-list-tile>
               {{ patient.dni }}
+            </v-list-tile>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-list-tile>      
+              {{ patient.last_name }}
             </v-list-tile>
           </v-list-tile-content>
         </v-list-tile>
