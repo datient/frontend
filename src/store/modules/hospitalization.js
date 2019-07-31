5f6207f7d76b97cca6c7ab73bf578a3b75494260
Benjamin Gilardini <benja.gilardini@gmail.com>
2019-07-31 09:56:40 -0300

    Update obtainHospitalization action

--- src/store/modules/hospitalization.js
+++ src/store/modules/hospitalization.js
@@ -7,33 +7,30 @@ const state = {
 
 const mutations = {
   setDoctor(state, doctor) {
-    console.log(doctor)
     state.doctor = doctor
   }
 }
 
 const actions = {
   obtainHospitalization({ commit, dispatch }, { token, bedId }) {
+    axios({
+      method: 'get',
+      url: `http://127.0.0.1:8000/api/hospitalization/${bedId}/bed_filter/`,
+      headers: { 'Authorization': `JWT ${token}` },
+    })
+    .then(res => {
+      let doctorId = res.data.doctor
+      let dni = res.data.patient
+      dispatch('patient/obtainPatient', { token, dni }, { root: true })
       axios({
         method: 'get',
-        url: `http://127.0.0.1:8000/api/bed/${bedId}/`,
+        url: `http://127.0.0.1:8000/api/doctor/${doctorId}/`,
         headers: { 'Authorization': `JWT ${token}` },
       })
       .then(res => {
-        let index = res.data.hospitalizations.length - 1
-        let dni = res.data.hospitalizations[index]['patient']
-        let doctor = res.data.hospitalizations[index]['doctor']
-        dispatch('patient/obtainPatient', { token, dni }, { root:true })
-        let email = res.data.email
-        axios({
-          method: 'get',
-          url: `http://127.0.0.1:8000/api/doctor/${doctor}/`,
-          headers: { 'Authorization': `JWT ${token}` },
-        })
-        .then(res => {
-          commit('setDoctor', res.data)
-        })
+        commit('setDoctor', res.data)
       })
+    })
   }
 }
 export default {
