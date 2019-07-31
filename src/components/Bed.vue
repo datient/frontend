6dbd6db0dccf96d991b90920bdd8d6884f7f9d9e
Benjamin Gilardini <benja.gilardini@gmail.com>
2019-07-31 11:15:58 -0300

    Display doctor in charge

--- src/components/Bed.vue
+++ src/components/Bed.vue
@@ -1,124 +1,29 @@
 <template>
   <div id="room">
-    <v-card round class="cardBed">
-      <v-toolbar class="toolbarSalas">
-          <v-toolbar-title class="titleSalas">Cama {{ bedId }}</v-toolbar-title>
-          <v-card-title>Medico a cargo: {{ hospitalization.doctor.last_name }} {{hospitalization.doctor.first_name}} </v-card-title>
-      <div class="text-xs-center">
-        <v-dialog
-          v-model="dialog"
-          width="500">
-          <v-card>
-            <v-card-title
-              class="headline grey lighten-2"
-              primary-title
-            >
-              Agregar un paciente
-            </v-card-title>
-            <v-container fluid>
-              <v-layout wrap>
-                <v-flex xs12>
-                  <v-combobox
-                    v-model="select"
-                    :items="patient.last_name"
-                    label="Seleccione uno de los pacientes disponibles para anadirlo a la cama"
-                  ></v-combobox>
-                </v-flex>      
-              </v-layout>
-            </v-container>
-           
-            <v-divider></v-divider>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="primary"
-                flat
-                @click="dialog = false"
-              >
-                Agregar
-              </v-btn>                                                                                                                                      
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </div>
-    </v-toolbar>
-    <div id="room">
+    <!-- <v-card round class="cardBed"> -->
+    <v-card class="cardBed">
+      <v-toolbar dense>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Cama {{ bedId }}</v-toolbar-title>
+       
+        <v-spacer></v-spacer>
+        
+        <v-icon>person</v-icon>
+        <v-toolbar-title>{{ patient.first_name }} {{ patient.last_name }}</v-toolbar-title>
+      </v-toolbar>  
+     
       <v-list>
         <v-container grid-list-md>
           <v-layout>
-            <v-flex xs3>
-              <v-card-text>Nombre Completo</v-card-text>
-            </v-flex>
-            <v-flex xs6 grid-list-md text-xs-center>
-              <v-card dark tile flat color="#2e7d32">
-                <v-card-text>{{ patient.first_name }} {{ patient.last_name }}</v-card-text>
-              </v-card>
+            <v-flex xs6>
+              <v-card-text >Nombre Completo: </v-card-text>
             </v-flex>
           </v-layout>
-          <v-layout>
-            <v-flex xs3>           
-              <v-card-text>Edad</v-card-text>
+          <v-flex xs6>
+              <v-card-text > {{ patient.last_name }} </v-card-text>
             </v-flex>
-            <v-flex xs6 grid-list-md text-xs-center>
-              <v-card dark tile flat color="#388e3c">
-                <v-card-text>{{ patient.age }}</v-card-text>
-              </v-card>
-            </v-flex>
-          </v-layout>
-          <v-layout>
-            <v-flex xs3>
-              <v-card-text>Dni</v-card-text>
-            </v-flex>
-            <v-flex xs6 grid-list-md text-xs-center>
-              <v-card dark tile flat color="#2e7d32">
-                <v-card-text>{{ patient.dni }}</v-card-text>
-              </v-card>
-            </v-flex>
-          </v-layout>
-          <v-layout>
-            <v-flex xs3>
-              <v-card-text>Fecha de Nacimiento</v-card-text>
-            </v-flex>
-            <v-flex xs6 grid-list-md text-xs-center>
-              <v-card dark tile flat color="#388e3c">
-                <v-card-text>{{ patient.birth_date }}</v-card-text>
-              </v-card>
-            </v-flex>
-          </v-layout>
-          <v-layout>
-            <v-flex xs3>
-              <v-card-text>Genero del Paciente</v-card-text>
-            </v-flex>
-            <v-flex xs6 grid-list-md text-xs-center>
-              <v-card dark tile flat color="#2e7d32">
-                <v-card-text>{{ patient.gender }}</v-card-text>
-              </v-card>
-            </v-flex>
-          </v-layout>
-          <v-layout>
-            <v-flex xs3>
-              <v-card-text>Edad</v-card-text>
-            </v-flex>
-            <v-flex xs6 grid-list-md text-xs-center>
-              <v-card dark tile flat color="#388e3c">
-                <v-card-text>{{ patient.age }}</v-card-text>
-              </v-card>
-            </v-flex>
-          </v-layout>
-          <v-layout>
-            <v-flex xs3>
-              <v-card-text>Diagnostico</v-card-text>
-            </v-flex>
-            <v-flex xs6 grid-list-md text-xs-center>
-              <v-card dark tile flat color="#2e7d32">
-                <v-card-text>{{ patient.income_diagnosis }}</v-card-text>
-              </v-card>
-            </v-flex>
-          </v-layout>
         </v-container>  
       </v-list>
-      </div>
     </v-card>
   </div>
 </template>
@@ -136,7 +41,7 @@ export default {
     }
   },
   computed: {
-    ...mapState(['hospitalization' ,'patient', 'user'])
+    ...mapState(['hospitalization', 'patient', 'user'])
   },
   mounted() {
     let bedId = this.bedId
