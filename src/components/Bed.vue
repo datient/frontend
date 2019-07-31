7e5cacc50457e0baaa77a9c3bb43d0e4ee137c96
Benjamin Gilardini <benja.gilardini@gmail.com>
2019-07-31 11:48:37 -0300

    Display patient in bed

--- src/components/Bed.vue
+++ src/components/Bed.vue
@@ -9,21 +9,78 @@
         <v-spacer></v-spacer>
         
         <v-icon>person</v-icon>
-        <v-toolbar-title>{{ patient.first_name }} {{ patient.last_name }}</v-toolbar-title>
+        <v-toolbar-title>{{ hospitalization.doctor.first_name }} {{ hospitalization.doctor.last_name }}</v-toolbar-title>
       </v-toolbar>  
-     
-      <v-list>
-        <v-container grid-list-md>
-          <v-layout>
-            <v-flex xs6>
-              <v-card-text >Nombre Completo: </v-card-text>
+      <v-container grid-list-xl text-center>
+        <v-layout>
+          <v-flex xs3 offset-xs3>
+              <v-card-text >Nombre: {{ patient.last_name }} {{ patient.first_name }}</v-card-text>
+          </v-flex>
+          <v-flex xs3>
+              <v-card-text >Edad: {{ patient.age }}</v-card-text>
+          </v-flex>
+        </v-layout>
+        <v-layout>
+          <v-flex xs3 xs3 offset-xs3>
+              <v-card-text >Dni: {{ patient.dni }}</v-card-text>
+          </v-flex>
+          <v-flex xs3>
+              <v-card-text >Fecha de Nacimiento: {{ patient.birth_date }}</v-card-text>
+          </v-flex>
+        </v-layout>
+        <v-layout>
+          <v-flex xs3 xs3 offset-xs3>
+              <v-card-text >Genero: {{ patient.gender }}</v-card-text>
+          </v-flex>
+          <v-flex xs3>
+              <v-card-text >Diagnostico: {{ patient.income_diagnosis }}</v-card-text>
+          </v-flex>
+        </v-layout>
+
+        <!-- <v-layout>
+          <v-flex xs3>
+              <v-card-text >Nombre:</v-card-text>
+          </v-flex>
+          <v-flex xs3>
+              <v-card-text >{{ patient.last_name }} {{ patient.first_name }}</v-card-text>
+          </v-flex>
+            <v-flex xs3>
+                <v-card-text >Edad:</v-card-text>
+            </v-flex>
+            <v-flex xs3>
+                <v-card-text >{{ patient.age }}</v-card-text>
             </v-flex>
           </v-layout>
-          <v-flex xs6>
-              <v-card-text > {{ patient.last_name }} </v-card-text>
+          <v-layout>
+            <v-flex xs3>
+                <v-card-text >Dni:</v-card-text>
             </v-flex>
-        </v-container>  
-      </v-list>
+            <v-flex xs3>
+                <v-card-text >{{ patient.dni }}</v-card-text>
+            </v-flex>
+            <v-flex xs3>
+                <v-card-text >Fecha de Nacimiento:</v-card-text>
+            </v-flex>
+            <v-flex xs3>
+                <v-card-text >{{ patient.birth_date }}</v-card-text>
+            </v-flex>
+          </v-layout>
+          <v-layout>
+            <v-flex xs3>
+                <v-card-text >Genero:</v-card-text>
+            </v-flex>
+            <v-flex xs3>
+                <v-card-text >{{ patient.gender }}</v-card-text>
+            </v-flex>
+            <v-flex xs3>
+                <v-card-text >Diagnostico:</v-card-text>
+            </v-flex>
+            <v-flex xs3>
+                <v-card-text >{{ patient.income_diagnosis }}</v-card-text>
+            </v-flex>
+          </v-layout> -->
+      </v-container>  
+    
     </v-card>
   </div>
 </template>
